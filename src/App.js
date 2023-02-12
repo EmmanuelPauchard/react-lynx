2b43a5fffa7d8465c9fa118454621afab97e26f9
Emmanuel Pauchard <emmanuel.pauchard@gmail.com>
2023-02-12 05:30:01 +0100

    Several improvements:
    - Improve responsiveness
    - Add Reset and Next buttons
    - Add comments

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+/**
+ * React Lynx game
+ * In this game, a minimum of 2 players compete to be the first to locate a particular image from a set of other images.
+ *
+ * This implementation is serverless.
+ *
+ * It needs a Flickr API key to get the images.
+ */
 import React, { useState, useEffect } from 'react';
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Dropdown from 'react-bootstrap/Dropdown';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Container from 'react-bootstrap/Container';
-
 import Image from 'react-bootstrap/Image'
+import { Button } from 'react-bootstrap';
 
+/**
+ * The game difficulty depends on the number of images displayed
+ */
 const lynxDifficulty = [
   {
     "str": "easy",
@@ -27,7 +36,7 @@ const lynxDifficulty = [
   }
 ];
 
-/** Return a random element from the list l */
+/** Return a random element from a list */
 function pickRandomImage(l) {
   return l.at(Math.floor(Math.random() * l.length));
 }
@@ -35,43 +44,30 @@ function pickRandomImage(l) {
 /**
  * The Menu bar:
  * - display and change difficulty
- * - restart TODO
+ * - game controls: next image, reset images, status
+ * - target image
  * - player names/scores TODO
  */
-function LynxMenu({ currentDifficulty, setDifficulty, image }) {
+function LynxMenu({ currentDifficulty, setDifficulty, image, status, next, reset}) {
 
   return (
 
-    // <div class="menu d-flex flex-row justify-content-between bg-primary">
-    //   <div class="p-2">Lynx</div>
-    //   <Dropdown>
-    //     <Dropdown.Toggle variant="success" id="dropdown-basic">
-    //       {"Difficulty: " + currentDifficulty.str}
-    //     </Dropdown.Toggle>
-
-    //     <Dropdown.Menu>
-    //       <Dropdown.Item>Easy</Dropdown.Item>
-    //       <Dropdown.Item>Medium</Dropdown.Item>
-    //       <Dropdown.Item>Hard</Dropdown.Item>
-    //     </Dropdown.Menu>
-    //   </Dropdown>
-
-    //   <Image src={image} />
-    // </div>
-
-    <Navbar className="p-2 align-content-center" bg="primary" variant="dark" expand="lg">
-      <Nav className="flex-column col-6 pt-0 justify-content-start">
-        <Navbar.Brand>Lynx</Navbar.Brand>
+    <Navbar id="nav-menu" className="p-2 flex-row justify-content-between" bg="primary" variant="dark" expand="lg">
+      <Nav className="flex-column pt-0 justify-content-start col-6 col-sm-2">
+        <Navbar.Brand id="nav-brand" onClick={reset}>Lynx</Navbar.Brand>
         <NavDropdown title={"Difficulty: " + currentDifficulty.str} id="basic-nav-dropdown">
           {lynxDifficulty.map((x) =>
             <NavDropdown.Item key={x.str} className={currentDifficulty === x ? "active" : ""} onClick={() => setDifficulty(x)}>{x.str}
             </NavDropdown.Item>
           )}
         </NavDropdown>
+        <div id="nav-status">{status}</div>
       </Nav>
 
-      <Nav className="flex-row col-6 justify-content-end">
-        <Image className="target" src={image} />
+      <Image id="nav-target" className="target col-sm-3 col-6" src={image} />
+      <Nav id="nav-controls" className="flex-row flex-sm-column col-12 col-sm-2">
+        <Button variant="success" onClick={next}>Next</Button>
+        <Button variant="danger" onClick={reset}>Reset</Button>
       </Nav>
     </Navbar>
   );
@@ -84,10 +80,9 @@ function ImageList({ urls, target, selected, handleClick }) {
   return (
     <div className="image-grid">
       {urls.map(x =>
-        <Image className={"fluid images col-2-lg " + (selected === x ? (target === selected ? "hit" : "miss") : "")} key={x.key} src={x.url} onClick={() => handleClick(x)} />)}
+        <Image className={"fluid images col-6 col-md-3 " + (selected === x ? (target === selected ? "hit" : "miss") : "")} key={x.key} src={x.url} onClick={() => handleClick(x)} />)}
     </div>);
 }
-
 
 function App() {
 
@@ -95,7 +90,9 @@ function App() {
   const [difficulty, setDifficulty] = useState(lynxDifficulty[1]);
   const [targetPic, setTargetPic] = useState();
   const [selectedPic, setSelectedPic] = useState();
-  const [status, setStatus] = useState("Loading");
+  const [status, setStatus] = useState("Loading...");
+  const [game, setGame] = useState(0);
+
 
   useEffect(() => {
     const page = Math.round(Math.random() * 10);
@@ -112,21 +109,24 @@ function App() {
             console.log("Error: no photos: ", data);
           }
           //const photo_array = data.photos.photo;
-          const photo_array = data.photos.photo.sort((a, b) => 0.5 - Math.random()).slice(0, difficulty.tiles_nb);
+          const photo_array = data.photos.photo.sort(() => 0.5 - Math.random()).slice(0, difficulty.tiles_nb);
           const updated_urls = photo_array.map(el => {
             return {
               "url": `https://live.staticflickr.com/${el.server}/${el.id}_${el.secret}_q.jpg`,
               "key": el.id
             };
           });
+          setSelectedPic("")
           setUrls(updated_urls);
           setTargetPic(pickRandomImage(updated_urls));
+          setStatus("Fight!")
+          console.log("Fetch done");
         },
         (error) => {
           console.log("Exception while fetching resources: ", error);
         }
       )
-  }, [difficulty])
+  }, [difficulty, game])
 
   /**
    * Check if selected image corresponds to target picture.
@@ -134,13 +134,31 @@ function App() {
    * @return: true on match, false otherwise
    */
   function validatePick(img) {
-    return (img === targetPic.key);
+    const success = (img === targetPic);
+    setStatus(success ? "Great Job!" : "Try Again");
+    setSelectedPic(img);
   };
+
+  /**
+   * Update some phony state used as the effect dependancy to force re-render
+   */
+  function reset() {
+    setGame(game + 1);
+  }
+
+  /**
+   * Select another target picture from the set and reset game status
+   */
+  function next() {
+    setSelectedPic("");
+    setStatus("Fight!")
+    setTargetPic(pickRandomImage(urls));
+  }
 
   return (
     <Container>
-      <LynxMenu setDifficulty={setDifficulty} currentDifficulty={difficulty} image={targetPic ? targetPic.url : ""}/>
-      <ImageList urls={urls} target={targetPic} selected={selectedPic} handleClick={setSelectedPic} />
+      <LynxMenu setDifficulty={setDifficulty} currentDifficulty={difficulty} image={targetPic ? targetPic.url : ""} status={status} next={next} reset={reset}/>
+      <ImageList urls={urls} target={targetPic} selected={selectedPic} handleClick={validatePick} />
     </Container>
   );
 }
