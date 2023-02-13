bfb4f1b6126a4d9b5e22e72a76b1b9fca918d871
Emmanuel Pauchard <emmanuel.pauchard@gmail.com>
2023-02-13 23:38:26 +0100

    ImageQuery: make API key a parameter

--- src/ImageQuery.mjs
+++ src/ImageQuery.mjs
@@ -1,31 +1,36 @@
+import default_images_urls from "./default_images.js";
+
 /**
- * Return #number images
+ * * Return #number images
+ * @param api_key {string} The service API key. If unset, default images are used.
  * @return {[{"url": string, "key": string}]}
  */
-async function fetchImages(number) {
-  console.log("Fetching ", number);
-  const page = Math.round(Math.random() * 10);
-  const perpage = 50;
-  const FLICKR_API = process.env.LYNX_FLICKR_API_KEY;
-  const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${FLICKR_API}&group_id=1422009%40N23&per_page=${perpage}&page=${page}&format=json&nojsoncallback=1`;
+async function fetchImages(number=24, api_key="") {
   let updated_urls = [];
+  if (api_key !== "") {
+    const page = Math.round(Math.random() * 10);
+    const perpage = 50;
+    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&group_id=1422009%40N23&per_page=${perpage}&page=${page}&format=json&nojsoncallback=1`;
 
-  try {
-    const resp = await fetch(url);
-    const data = await resp.json();
-    if (!("photos" in data)) {
-      console.log("Error: no photos: ", data);
-      return [];
+    try {
+      const resp = await fetch(url);
+      const data = await resp.json();
+      if (!("photos" in data)) {
+        console.log("Error: no photos: ", data);
+        return [];
+      }
+      const photo_array = data.photos.photo.sort(() => 0.5 - Math.random()).slice(0, number);
+      updated_urls = photo_array.map(el => {
+        return {
+          "url": `https://live.staticflickr.com/${el.server}/${el.id}_${el.secret}_q.jpg`,
+          "key": el.id
+        };
+      });
+    } catch (error) {
+      console.log("Exception while fetching resources: ", error);
     }
-    const photo_array = data.photos.photo.sort(() => 0.5 - Math.random()).slice(0, number);
-    updated_urls = photo_array.map(el => {
-      return {
-        "url": `https://live.staticflickr.com/${el.server}/${el.id}_${el.secret}_q.jpg`,
-        "key": el.id
-      };
-    });
-  } catch (error) {
-    console.log("Exception while fetching resources: ", error);
+  } else {
+    updated_urls = default_images_urls;
   }
 
   return updated_urls;
---
+++ src/default_images.js
@@ -0,0 +1,100 @@
+const default_images_urls = [
+  {
+    url: 'https://live.staticflickr.com/65535/52665686999_b5612ae611_q.jpg',
+    key: '52665686999'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52665744244_9b2430b33d_q.jpg',
+    key: '52665744244'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52663440349_525df4b6c0_q.jpg',
+    key: '52663440349'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52664829227_ba1298bdfb_q.jpg',
+    key: '52664829227'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52666227006_8520c211ba_q.jpg',
+    key: '52666227006'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52664864052_b575102b68_q.jpg',
+    key: '52664864052'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52665094741_b8b45438eb_q.jpg',
+    key: '52665094741'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52664841265_98fd9193b1_q.jpg',
+    key: '52664841265'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52664918893_980f8e83aa_q.jpg',
+    key: '52664918893'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52664948705_9408d125e8_q.jpg',
+    key: '52664948705'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52664102597_dcd1116d98_q.jpg',
+    key: '52664102597'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52664876950_5b3a50bb45_q.jpg',
+    key: '52664876950'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52663594260_82bb99ed24_q.jpg',
+    key: '52663594260'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52664262794_3ddd7ac731_q.jpg',
+    key: '52664262794'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52665765075_a789d29a70_q.jpg',
+    key: '52665765075'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52663595035_36853c4a1f_q.jpg',
+    key: '52663595035'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52663568451_68e78f05be_q.jpg',
+    key: '52663568451'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52663748266_c2d5120a4a_q.jpg',
+    key: '52663748266'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52665362234_626357dc60_q.jpg',
+    key: '52665362234'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52665657154_2c8d84fe58_q.jpg',
+    key: '52665657154'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52664548257_872e500e98_q.jpg',
+    key: '52664548257'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52664643649_b601aa8f49_q.jpg',
+    key: '52664643649'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52665717040_a5a1060e75_q.jpg',
+    key: '52665717040'
+  },
+  {
+    url: 'https://live.staticflickr.com/65535/52664277303_64f7eea496_q.jpg',
+    key: '52664277303'
+  }
+];
+
+export default default_images_urls;
