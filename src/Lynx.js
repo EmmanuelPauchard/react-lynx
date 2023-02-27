b48991e645af471eab193dea1ce3e48da6ec5c56
Emmanuel Pauchard <emmanuel.pauchard@gmail.com>
2023-02-27 16:40:14 +0100

    WIP: remove all useState, switch to useReducer
    This does not fully work yet.
    App is re-rendered too often.
    State seems inconsistently used by targetPic, so the target is not in
    the set!

--- src/Lynx.js
+++ src/Lynx.js
@@ -6,7 +6,7 @@
  *
  * It needs a Flickr API key to get the images.
  */
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 
 import './Lynx.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -43,22 +43,49 @@ function pickRandomImage(l) {
 
 function Lynx() {
 
-  const [urls, setUrls] = useState([]);
-  const [difficulty, setDifficulty] = useState(lynxDifficulty[1]);
-  const [targetPic, setTargetPic] = useState();
-  const [selectedPic, setSelectedPic] = useState();
-  const [status, setStatus] = useState("Loading...");
-  const [game, setGame] = useState(0);
-  const [api, setApi] = useState("");
+  const initialState = {
+    urls: [],
+    difficulty: lynxDifficulty[1],
+    targetPic: null,
+    selectedPic: null,
+    status: "Loading...",
+    game: 0,
+    api: ""
+  };
+
+  function reducer(state, action) {
+    let {[action.target]: current} = state;
+    // if (current === undefined) {
+    //   console.log("Error: can't handle state update: ", action);
+    //   return state;
+    // }
+
+    switch(action.target) {
+      case "game":
+        // "Game" is our only state variable whose associated action is stateful
+        current += 1;
+        break;
+      default:
+        // All other states: just set the new payload
+        current = action.payload;
+        break;
+    }
+
+    return {...state, [action.target]: current};
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    fetchImages(difficulty.tiles_nb, api).then((updated_urls) => {
-      setSelectedPic("");
-      setUrls(updated_urls);
-      setTargetPic(pickRandomImage(updated_urls));
-      setStatus("Fight!");
+    // const getImages = async () => {
+    //   return await fetchImages(state.difficulty.tiles_nb, state.api);
+    // }
+    fetchImages(state.difficulty.tiles_nb, state.api).then((updated_urls) => {
+      // const updated_urls = getImages();
+      dispatch({target: "urls", payload: updated_urls});
+      next();
     })
-  }, [difficulty, game, api])
+  }, [state.game, state.api, state.difficulty]);
 
   /**
    * Check if selected image corresponds to target picture.
@@ -66,39 +93,41 @@ function Lynx() {
    * @return: true on match, false otherwise
    */
   function validatePick(img) {
-    const success = (img === targetPic);
-    setStatus(success ? "Great Job!" : "Try Again");
-    setSelectedPic(img);
+    const success = (img === state.targetPic);
+    dispatch({target: "status", payload: success ? "Great Job!" : "Try Again"});
+    dispatch({target: "selectedPic", payload: img});
   };
 
   /**
    * Update some phony state used as the effect dependancy to force re-render
    */
   function reset() {
-    setGame(game + 1);
+    dispatch({target: "game"});
   }
 
   /**
    * Select another target picture from the set and reset game status
    */
   function next() {
-    setSelectedPic("");
-    setStatus("Fight!")
-    setTargetPic(pickRandomImage(urls));
+    dispatch({target: "selectedPic", payload: ""});
+    dispatch({target: "status", payload: "Fight!"});
+    dispatch({target: "targetPic", payload: pickRandomImage(state.urls)});
   }
+
+  console.log("Current state: ", state);
 
   return (
     <Container>
-      <LynxMenu setDifficulty={setDifficulty} currentDifficulty={difficulty} difficulties={lynxDifficulty} image={targetPic ? targetPic.url : ""} status={status} next={next} reset={api === "" ? null : reset}/>
+      <LynxMenu next={next} reset={reset} setDifficulty={(v) => dispatch({target: "difficulty", payload: v})} difficulties={lynxDifficulty} currentDifficulty={state.difficulty} image={state.targetPic ? state.targetPic.url : ""} status={state.status}/>
       {
-        urls.length >0 ?
-          <ImageList urls={urls} target={targetPic} selected={selectedPic} handleClick={validatePick} />
+        state.urls.length >0 ?
+          <ImageList urls={state.urls} target={state.targetPic} selected={state.selectedPic} handleClick={validatePick} />
         : <div className="alert alert-danger">No images. Please check your API key.</div>
       }
       <hr/>
       <div>
-        <div>{api === "" ? "Using predefined Flickr Images; Add your API key to get new random images from Flickr" : `Using Flickr Api Key ${api}`}</div>
-        <ApiInput handler={setApi} value={api}/>
+        <div>{state.api === "" ? "Using predefined Flickr Images; Add your API key to get new random images from Flickr" : `Using Flickr Api Key ${state.api}`}</div>
+        <ApiInput handler={(v) => dispatch({target: "api", payload: v})} value={state.api}/>
       </div>
     </Container>
   );
